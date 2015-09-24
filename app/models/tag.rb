e336d30131c67fe32d22bba0cbba482b28a9c3c3
Aaron <aarongkendall@gmail.com>
2015-09-24 12:01:37 +0100

    tags searchable

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,10 +1,11 @@
 require 'data_mapper'
-# require 'dm-postgres-adapter'
+require 'dm-postgres-adapter'
 
 class Tag
   include DataMapper::Resource
 
-  property :id,   Serial
-  property :name, String
+  property :id,           Serial
+  property :name,         String
+  has n, :links, through: Resource
 
 end
--- data_mapper_setup.rb
+++ data_mapper_setup.rb
@@ -3,8 +3,6 @@ require 'data_mapper'
 
 env = ENV['RACK_ENV'] || 'development'
 
-p env
-
 # we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{env}")
 
