31d686edf8bda5cf69c5123e2670f9f47ba18067
Archie Fielding <archiefielding@gmail.com>
2015-09-24 17:39:06 +0100

    user login & password

--- app/app.rb
+++ app/app.rb
@@ -5,6 +5,9 @@ require 'byebug'
 class BookmarkManager < Sinatra::Base
   set :views, proc{File.join(root, '..' , 'views')}
 
+  enable :sessions
+  set :session_secret, 'super secret'
+
   get '/' do
     redirect '/links'
   end
@@ -32,8 +35,26 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/tags/:name' do
-    tag = Tag.first(name: params[:name])
+    tag = Tag.all(name: params[:name])
     @links = tag ? tag.links : []
     erb :'links/index'
   end
+
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/users' do
+    user = User.create(email: params[:email],
+                password: params[:password])
+    session[:user_id] = user.id
+    redirect to('/links')
+  end
+
+  helpers do
+    def current_user
+      User.get(session[:user_id])
+    end
+  end
+
 end
